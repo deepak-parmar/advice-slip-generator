93f1ae5671ebf67df87a6286bd7aa9ffd8fc7af1
Deepak Parmar <dgparmar1999@rediffmail.com>
2022-02-21 11:53:06 +0530

    Create _document.js
    Create _document.js for common webpage contents.
    
    - Import Google fonts
    - Style `body` and set font to `body`

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+
+class MyDocument extends Document {
+  render = () =>
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href={"https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap"} rel="stylesheet" />
+      </Head>
+      <body className={"bg-[#202632] h-screen grid place-content-center"} style={{fontFamily: '"Manrope", "sans-serif"'}}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+}
+
+export default MyDocument
