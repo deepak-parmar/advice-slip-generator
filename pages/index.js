5c94f63a340d2e6c928b1c15469bb8a9764bff91
deepak-parmar <dgparmar1999@rediffmail.com>
2022-04-19 18:40:23 +0530

    Add comments and clean code

--- pages/index.js
+++ pages/index.js
@@ -8,71 +8,69 @@ import mobileDivider from "../public/images/pattern-divider-mobile.svg"
 
 class AdviceSlipGenerator extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      id: null, advice: null
-    }
-    this.fetchNewQuote = this.fetchNewQuote.bind(this)
-  }
-
-  // Fetches quote on load
-  componentDidMount = () => this.fetchNewQuote()
-  
-  // Fetches new quote
-  fetchNewQuote = () => {
-    const refreshBtn = document.getElementById("refreshBtn")
-    refreshBtn.classList.add("animate-spin")
-    
-    const state = this
-    fetch("https://api.adviceslip.com/advice")
-      .then(response => response.json())
-      .then(data => {
-        state.setState({
-          id: data.slip.id,
-          advice: data.slip.advice
-        })
-      })
-      .catch(() => {
-        alert("Something went wrong! Try again.")
-      });
-
-    refreshBtn.classList.remove("animate-spin")
+	constructor(props) {
+		super(props)
+		this.state = { id: null, advice: null }
+		this.fetchNewQuote = this.fetchNewQuote.bind(this)
 	}
 
-  render = () =>
-  <>
-  <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Advice Generator App | Frontend Mentor Challenge</title>
-  </Head>
+	// Fetches quote on load
+	componentDidMount = () => this.fetchNewQuote()
+	
+	// Fetches new quote
+	fetchNewQuote = () => {
+		const refreshBtn = document.getElementById("refreshBtn")
+		refreshBtn.classList.add("animate-spin")
+		
+		const state = this
+		fetch("https://api.adviceslip.com/advice")
+			.then(response => response.json())
+			.then(data => {
+				state.setState({
+					id: data.slip.id,
+					advice: data.slip.advice
+				})
+			})
+			.catch(() => {
+				alert("Something went wrong! Try again.")
+			});
 
-  <main className={"flex flex-col items-center p-4"}>
-    <section className={"bg-[#313a49] max-w-[32rem] min-h-[20rem] p-6 lg:p-12 rounded-xl flex flex-col justify-center items-center"}>
-      
-      {/* advice id */}
-      <h1 className={"text-[#52ffa8] uppercase text-[11px] tracking-[4.8px] pb-6"}>Advice #{this.state.id}</h1>
-      
-      {/* advice */}
-      <q className={"text-[#cee3e9] leading-1 text-[1.54rem] text-center tracking-wide pb-6"}>
-        {this.state.advice}
-      </q>
-      
-      {/* Desktop divider */}
-      <div className={"hidden lg:block"}>
-        <Image src={desktopDivider} width={415.81} height={14.98} alt="" />
-      </div>
+		refreshBtn.classList.remove("animate-spin")
+	}
 
-      {/* Mobile divider */}
-      <div className={"block lg:hidden"}>
-        <Image src={mobileDivider} width={295} height={16} alt="" />
-      </div>
-    </section>
-    <button id="refreshBtn" className={"bg-[#52ffa8] p-5 rounded-full w-[3.875rem] h-[3.875rem] -mt-8 transition ease-in-out duration-75 hover:drop-shadow-[0_0_18px_rgb(82,255,168)]"} onClick={this.fetchNewQuote}>
-      <Image src="/images/icon-dice.svg" width="22" height="22" alt="Refresh advice button" />
-    </button>
-  </main>
-  </>
+	render = () =>
+	<>
+	<Head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Advice Generator App | Frontend Mentor Challenge</title>
+	</Head>
+
+	<main className={"flex flex-col items-center p-4"}>
+		<section className={"bg-[#313a49] max-w-[32rem] min-h-[20rem] p-6 lg:p-12 rounded-xl flex flex-col justify-center items-center"}>
+			
+			{/* advice id */}
+			<h1 className={"text-[#52ffa8] uppercase text-[11px] tracking-[4.8px] pb-6"}>Advice #{this.state.id}</h1>
+			
+			{/* advice */}
+			<q className={"text-[#cee3e9] leading-1 text-[1.54rem] text-center tracking-wide pb-6"}>
+				{this.state.advice}
+			</q>
+			
+			{/* Desktop divider */}
+			<div className={"hidden lg:block"}>
+				<Image src={desktopDivider} width={415.81} height={14.98} alt="" />
+			</div>
+
+			{/* Mobile divider */}
+			<div className={"block lg:hidden"}>
+				<Image src={mobileDivider} width={295} height={16} alt="" />
+			</div>
+		</section>
+		<button id="refreshBtn" className={"bg-[#52ffa8] p-5 rounded-full w-[3.875rem] h-[3.875rem] -mt-8 transition ease-in-out duration-75 hover:drop-shadow-[0_0_18px_rgb(82,255,168)]"} onClick={this.fetchNewQuote}>
+			<Image src="/images/icon-dice.svg" width="22" height="22" alt="Refresh advice button" />
+		</button>
+	</main>
+	</>
 
 }
 
