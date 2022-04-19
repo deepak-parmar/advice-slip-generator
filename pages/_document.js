e900a2bcbe1f6162daba45f00b6e39b91a6f79b4
deepak-parmar <dgparmar1999@rediffmail.com>
2022-04-19 18:32:10 +0530

    Preload fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,15 @@ class MyDocument extends Document {
   render = () =>
     <Html lang="en">
       <Head>
-        <meta charSet="utf-8" />
+        <meta charSet="utf-8" />        
         <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preload" href={"https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap"} as="style" /> 
         <link href={"https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap"} rel="stylesheet" />
       </Head>
+
       <body className={"bg-[#202632] h-screen grid place-content-center"} style={{fontFamily: '"Manrope", "sans-serif"'}}>
         <Main />
         <NextScript />
